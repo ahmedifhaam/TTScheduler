c1ade6b2db18b7c5a53750e19cb506e0e5ec6970
XELVIAS-PC\Ahmed <ahmedifhaam1@gmail.com>
2017-04-13 11:28:20 +0530

    created models and half of GA

---
+++ src/timetable/scheduler/pkgfor/uokscience/TimeTableSchedulerForUOKScience.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package timetable.scheduler.pkgfor.uokscience;
+
+import java.util.ArrayList;
+import timetable.scheduler.pkgfor.uokscience.models.ClassRoom;
+import timetable.scheduler.pkgfor.uokscience.models.Day;
+import timetable.scheduler.pkgfor.uokscience.models.Population;
+import timetable.scheduler.pkgfor.uokscience.models.Subject;
+import timetable.scheduler.pkgfor.uokscience.models.TimeSlot;
+import timetable.scheduler.pkgfor.uokscience.models.TimeTable;
+
+/**
+ *
+ * @author Ahmed
+ */
+public class TimeTableSchedulerForUOKScience {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+       Population pop = new Population( 3,true);
+       
+    }
+    
+}
